bc4ef6f5b5606e266c61b3657c5170e1694509fa
Carlos Henrique <carlos.henrique0307200@gmail.com>
2020-08-02 19:18:14 -0300

    create logs for mobile test

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,32 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 
 import Player from './Player';
 
 function App() {
+  const [output, setOutput] = useState<string[]>([]);
   const play = () => {
-    console.log('PLAY:', new Date().toTimeString());
-  }
-
-  const onClickPlay = () => {
-    play();
+    const message = 'PLAY:' + new Date().toTimeString();
+    setOutput([...output, message]);
   }
 
   useEffect(() => {
     window.addEventListener("blur", () => {
       setTimeout(() => play(), 2000);
     });
-  }, [])
+  }, []);
+
+  const onClickPlay = () => {
+    play();
+  }
 
   return (
     <div className="l-app">
       <Player />
       <button className="c-app__btn" onClick={onClickPlay} type="button">PLAY</button>
+      <ul>
+        {output.map((message, i) => <li key={i}>{message}</li>)}
+      </ul>
     </div>
   );
 }
