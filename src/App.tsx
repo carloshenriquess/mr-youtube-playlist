0fcd570fe14aa6b45ed65cf222da4f3c4ed578ad
Carlos Henrique <carlos.henrique0307200@gmail.com>
2020-08-03 00:16:26 -0300

    create navigation in playlist

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,112 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
-
-import Player from './Player';
+import YouTube, { Options } from 'react-youtube';
 
 function App() {
-  const [output, setOutput] = useState<string[]>([]);
-  const play = () => {
-    const message = 'PLAY:' + new Date().toTimeString();
-    setOutput([...output, message]);
-  }
+  const initialVideo = 3;
+  const [ready, setReady] = useState<boolean>(false);
+  const [playerInitialized, setPlayerInitialized] = useState<boolean>(false);
+  const [player, setPlayer] = useState<YT.Player>();
+  const [playedVideos, setPlayedVideos] = useState<readonly number[]>([initialVideo]);
+  const [previousDisabled, setPreviousDisabled] = useState<boolean>(true);
+  const [currentVideo, setCurrentVideo] = useState<number>(initialVideo);
+  const options: Options = {
+    playerVars: {
+      autoplay: 1,
+    },
+  };
 
   useEffect(() => {
-    window.addEventListener("blur", () => {
-      setTimeout(() => play(), 2000);
-    });
-  }, []);
+    if (!player) { return; }
+    initializePlayer();
+  }, [player]);
 
-  const onClickPlay = () => {
-    play();
+  useEffect(() => {
+    if (!player) { return; }
+    player?.playVideoAt(currentVideo);
+  }, [currentVideo]);
+
+  useEffect(() => {
+    const currentVideoIsFirst = !!playedVideos.length && playedVideos[0] === currentVideo;
+    setPreviousDisabled(!ready || !playedVideos.length || currentVideoIsFirst);
+  }, [ready, playedVideos, currentVideo])
+
+  // useEffect(() => {
+  // window.addEventListener("blur", () => {
+  //   setTimeout(() => play(), 2000);
+  // });
+  // }, []);
+  const onReady = (event: YT.PlayerEvent) => {
+    setPlayer(event.target);
+  };
+
+  const onStateChange = (event: YT.OnStateChangeEvent) => {
+    if (playerInitialized && !ready) {
+      setReady(true);
+    }
+    if (event.data === 1) {
+      const duration = player?.getDuration() as number;
+      const currentTime = player?.getCurrentTime() as number;
+      const remainingTime = duration - currentTime;
+      setTimeout(() => {
+        player?.pauseVideo();
+        playNextVideo();
+      }, remainingTime * 1000);
+    }
+  };
+
+  const onClickNext = () => {
+    playNextVideo();
+  };
+
+  const onClickPrevious = () => {
+    const currentVideoIndex = playedVideos.findIndex(video => video === currentVideo);
+    if (currentVideoIndex > 0) {
+      setCurrentVideo(playedVideos[currentVideoIndex - 1]);
+    }
+  };
+
+  const initializePlayer = () => {
+    player?.loadPlaylist({
+      list: 'PLhHHziNjM2TPIOkQqPvRGMeg32YmasOVr',
+      index: 3,
+    });
+    setPlayerInitialized(true);
+  };
+
+  const playNextVideo = () => {
+    const videosLength = player?.getPlaylist().length || 0;
+    if (playedVideos.length === videosLength) {
+      setPlayedVideos([]);
+    }
+    const currentVideoIndex = playedVideos.findIndex(video => video === currentVideo);
+    if (currentVideoIndex >= 0 && currentVideoIndex !== playedVideos.length - 1) {
+      setCurrentVideo(playedVideos[currentVideoIndex + 1]);
+    } else {
+      const getNextIndex = (nextVideo: number): number => {
+        if (playedVideos.includes(nextVideo)) {
+          nextVideo++;
+          return getNextIndex(nextVideo === videosLength ? 0 : nextVideo);
+        }
+        return nextVideo;
+      }
+      const nextVideo = getNextIndex(Math.floor(Math.random() * videosLength));
+      setPlayedVideos([...playedVideos, nextVideo]);
+      setCurrentVideo(nextVideo);
+    }
   }
 
   return (
     <div className="l-app">
-      <Player />
-      <button className="c-app__btn" onClick={onClickPlay} type="button">PLAY</button>
-      <ul>
-        {output.map((message, i) => <li key={i}>{message}</li>)}
-      </ul>
+      <div className="c-player">
+        <div className="c-player__iframe-wrapper">
+          <YouTube className="c-player__iframe" opts={options} onReady={onReady} onStateChange={onStateChange} />
+        </div>
+        <div className="c-player__buttons">
+          <button className="c-player__btn" onClick={onClickPrevious} disabled={previousDisabled} type="button">PREVIOUS</button>
+          <button className="c-player__btn" onClick={onClickNext} disabled={!ready} type="button">NEXT</button>
+        </div>
+      </div>
     </div>
   );
 }
